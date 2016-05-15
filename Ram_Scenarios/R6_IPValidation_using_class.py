5f25a38c63fa5ba985e358e4fb199179153aaa52
ashokmoorthi <ashokmoorthims@gmail.com>
2016-05-15 13:47:16 -0400

    IP Validation using class

---
+++ Ram_Scenarios/R6_IPValidation_using_class.py
@@ -0,0 +1,7 @@
+''' Ram's scenario:
+1) Get input from User
+2) Validate if it is valid numeric inputs
+3) Validate if it is proper IP
+4) If Valid IP then what is the class it belongs to
+5) Do it using class
+'''
